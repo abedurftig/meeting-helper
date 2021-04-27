4a89349688467d0a74488e1722d67e54cc71955a
abedurftig <dasnervtdoch@gmail.com>
2021-04-27 10:50:12 +0200

    Install mint

--- build-script.sh
+++ build-script.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+# install mint
+wget --no-verbose -O mint https://mint-lang.s3-eu-west-1.amazonaws.com/mint-master-linux
+chmod +x ./mint
+sudo mv ./mint /usr/local/bin/mint
+
+# build
 cd web/meeting-helper
 mint build --skip-service-worker -e .env.production
 cd ../..
